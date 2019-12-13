003437d233944c19037b9943c9ec37b04eb91d08
LiamPKenna <liampkenna@gmail.com>
2019-12-13 08:26:36 -0800

    add test for clean and sort and comment out anagram test for later use

--- spec/tester_spec.rb
+++ spec/tester_spec.rb
@@ -4,7 +4,11 @@ require('tester')
 tester = Tester.new()
 
 describe('#Tester') do
-  it('should return "These are anagrams" if two input strings are anagrams') do
-    expect(tester.test('eat', 'tea')).to(eq("These are anagrams"))
+  # it('should return "These are anagrams" if two input strings are anagrams') do
+  #   expect(tester.test('eat', 'tea')).to(eq("These are anagrams"))
+  # end
+
+  it('') do
+    expect(tester.clean_and_sort("Dcb a!@ e")).to(eq('abcde'))
   end
 end
