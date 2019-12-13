8cc279bec203a5499aaf2452b2d0828c31f33dc8
LiamPKenna <liampkenna@gmail.com>
2019-12-13 08:46:36 -0800

    Add testing for a method that will check if a string contains non words

--- spec/tester_spec.rb
+++ spec/tester_spec.rb
@@ -8,7 +8,12 @@ describe('#Tester') do
     expect(tester.anagram_test('eat', 'tea')).to(eq("These are anagrams"))
   end
 
-  it('') do
+  it('should remove all non-alpha characters and sort to alphabetical order') do
     expect(tester.clean_and_sort("Dcb a!@ e")).to(eq('abcde'))
+  end
+
+  it('should test for the presence of non words') do
+    expect(tester.contains_non_word('this is a tst')).to(eq(true))
+    expect(tester.contains_non_word('this is a test')).to(eq(false))
   end
 end
