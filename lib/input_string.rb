13af5fe93d88a103a71224a6d59a2990db968580
LiamPKenna <liampkenna@gmail.com>
2019-12-13 12:07:02 -0800

    Add logic to check input words against dictionary file

--- lib/input_string.rb
+++ lib/input_string.rb
@@ -2,6 +2,7 @@ class InputString
   attr_reader(:string, :clean, :non_word)
   def initialize(input_string)
     @text = input_string
+    @all_words = File.open('./dict.txt').read.chomp.split(' ')
     @clean = clean_and_sort()
     @non_word = contains_non_word()
   end
@@ -14,7 +15,8 @@ class InputString
     word_array = @text.downcase.split(' ').map{ |s| s.gsub(/[^a-z]/, '') }
     no_vowels = word_array.any?{ |w| !/[aeiouy]+/.match(w) }
     trip_characters = word_array.any?{ |w| %r{(.)\1{2}}.match(w) }
-    (no_vowels || trip_characters)
+    not_word = word_array.any?{ |w| !(@all_words.include?(w)) }
+    (not_word || no_vowels || trip_characters)
   end
 
 end
