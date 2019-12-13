48e61089238d94b4f275e3d0abeb438894446583
LiamPKenna <liampkenna@gmail.com>
2019-12-13 08:36:23 -0800

    Uncomment anagram_test method test

--- spec/tester_spec.rb
+++ spec/tester_spec.rb
@@ -4,9 +4,9 @@ require('tester')
 tester = Tester.new()
 
 describe('#Tester') do
-  # it('should return "These are anagrams" if two input strings are anagrams') do
-  #   expect(tester.test('eat', 'tea')).to(eq("These are anagrams"))
-  # end
+  it('should return "These are anagrams" if two input strings are anagrams') do
+    expect(tester.anagram_test('eat', 'tea')).to(eq("These are anagrams"))
+  end
 
   it('') do
     expect(tester.clean_and_sort("Dcb a!@ e")).to(eq('abcde'))
