ed40b1228ae903495054cf5f2b5393e58c4460a8
LiamPKenna <liampkenna@gmail.com>
2019-12-13 10:09:16 -0800

    Refactor tests to allow for moving input_string logic to a new class

---
+++ spec/input_string_spec.rb
@@ -0,0 +1,17 @@
+require('rspec')
+require('input_string')
+describe('#InputString') do
+  it('should remove all non-alpha characters and sort to alphabetical order') do
+    tester = InputString.new("Dcb a!@ e")
+    expect(tester.clean_and_sort).to(eq('abcde'))
+  end
+
+  it('should test for the presence of non words') do
+    tester = InputString.new('this is a tst')
+    expect(tester.contains_non_word).to(eq(true))
+    tester = InputString.new('ttthis is a test')
+    expect(tester.contains_non_word).to(eq(true))
+    tester = InputString.new('this is a test')
+    expect(tester.contains_non_word).to(eq(false))
+  end
+end
--- spec/tester_spec.rb
+++ spec/tester_spec.rb
@@ -9,18 +9,6 @@ describe('#Tester') do
     expect(tester.anagram_test).to(eq("These are anagrams"))
   end
 
-  it('should remove all non-alpha characters and sort to alphabetical order') do
-    tester = Tester.new('eat', 'tea')
-    expect(tester.clean_and_sort("Dcb a!@ e")).to(eq('abcde'))
-  end
-
-  it('should test for the presence of non words') do
-    tester = Tester.new('eat', 'tea')
-    expect(tester.contains_non_word('this is a tst')).to(eq(true))
-    expect(tester.contains_non_word('ttthis is a test')).to(eq(true))
-    expect(tester.contains_non_word('this is a test')).to(eq(false))
-  end
-
   it('should test if strings are antigrams') do
     tester = Tester.new('word', 'pick')
     expect(tester.is_antigram).to(eq(true))
