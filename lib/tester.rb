f9bcf7bf9cea745612f1c4d3cd1e3155a05edfde
LiamPKenna <liampkenna@gmail.com>
2019-12-13 08:34:00 -0800

    add clean and sort logic to pass initial test

--- lib/tester.rb
+++ lib/tester.rb
@@ -1,4 +1,12 @@
 class Tester
   def initialize
   end
+
+  # def test(string1, string2)
+  #   is_anagram =
+  # end
+
+  def clean_and_sort(string)
+    string.downcase.split('').keep_if{ |x| /[a-z]/.match?(x)}.sort.join('')
+  end
 end
