c36791d6a2136562b94647640142f9b729b246dc
LiamPKenna <liampkenna@gmail.com>
2019-12-13 14:37:31 -0800

    Update test data name and add test for palindrome method

--- spec/input_string_spec.rb
+++ spec/input_string_spec.rb
@@ -3,16 +3,21 @@ require('input_string')
 
 describe('#InputString') do
   it('should remove all non-alpha characters and sort to alphabetical order') do
-    tester = InputString.new("Dcb a!@ e")
-    expect(tester.clean_and_sort).to(eq('abcde'))
+    string1 = InputString.new("Dcb a!@ e")
+    expect(string1.clean_and_sort).to(eq('abcde'))
   end
 
   it('should test for the presence of non words') do
-    tester = InputString.new('this is a tst')
-    expect(tester.contains_non_word).to(eq(true))
-    tester = InputString.new('ttthis is a test')
-    expect(tester.contains_non_word).to(eq(true))
-    tester = InputString.new('this is a test')
-    expect(tester.contains_non_word).to(eq(false))
+    string1 = InputString.new('this is a tst')
+    expect(string1.contains_non_word).to(eq(true))
+    string1 = InputString.new('ttthis is a test')
+    expect(string1.contains_non_word).to(eq(true))
+    string1 = InputString.new('this is a test')
+    expect(string1.contains_non_word).to(eq(false))
+  end
+
+  it('should form an array of any input words that are palindromes') do
+    string1 = InputString.new('bob is a toot')
+    expect(string1.get_palindrome).to(eq(['bob','toot']))
   end
 end
