52752c6334772a49c655031c9aed658e666a57cf
LiamPKenna <liampkenna@gmail.com>
2019-12-13 09:03:57 -0800

    Add contains_non_word logic checking for the absence of vowels

--- lib/tester.rb
+++ lib/tester.rb
@@ -2,11 +2,17 @@ class Tester
   def initialize
   end
 
-  # def test(string1, string2)
-  #   is_anagram =
-  # end
+  def anagram_test(string1, string2)
+    is_anagram = clean_and_sort(string1) == clean_and_sort(string2)
+    (is_anagram) ? 'These are anagrams' : 'These are not anagrams'
+  end
 
   def clean_and_sort(string)
     string.downcase.split('').keep_if{ |x| /[a-z]/.match?(x)}.sort.join('')
+  end
+
+  def contains_non_word(string)
+    word_array = string.downcase.split(' ').map{ |s| s.gsub(/[^a-z]/, '')}
+    word_array.any?{ |w| !/[aeiouy]+/.match(w) }
   end
 end
