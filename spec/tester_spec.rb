87097652803e725c83c8ee21709b810a8670556d
LiamPKenna <liampkenna@gmail.com>
2019-12-13 08:18:30 -0800

    Add initial test for anagram checking

--- spec/tester_spec.rb
+++ spec/tester_spec.rb
@@ -1,7 +1,10 @@
 require('rspec')
 require('tester')
+
+tester = Tester.new()
+
 describe('#Tester') do
-  it('') do
-    expect().to(eq())
+  it('should return "These are anagrams" if two input strings are anagrams') do
+    expect(tester.test('eat', 'tea')).to(eq("These are anagrams"))
   end
 end
