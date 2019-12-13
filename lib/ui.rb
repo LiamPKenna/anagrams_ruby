fe86aaf9358aa27b5d796d965837e9613f3d96fa
LiamPKenna <liampkenna@gmail.com>
2019-12-13 15:36:57 -0800

    Move user response branching to UI and refactor for readability

--- lib/ui.rb
+++ lib/ui.rb
@@ -4,15 +4,14 @@ class UI
   def initialize
     @break = make_break()
     @title = make_title()
-    @offset = '               '
   end
 
   def welcome
     puts @break
     puts @title
     puts @break
-    puts "#{@offset}Welcome to Anagram / Antigram!"
-    puts "#{@offset}   (press ENTER to begin)"
+    puts "               Welcome to Anagram / Antigram!"
+    puts "                  (press ENTER to begin)"
     puts @break
     gets
   end
@@ -30,15 +29,33 @@ class UI
 
   def test_strings(string1, string2)
     tester = Tester.new(string1, string2)
-    if tester.found_palindromes.length > 0
-      puts @break
-      puts "  You entered the following palindromes:"
-      puts "  #{tester.found_palindromes.join(', ')}"
+    puts @break
+    if tester.all_real_words
+      if tester.found_palindromes.length > 0
+        print('You entered the following palindromes:')
+        print("#{tester.found_palindromes.join(', ')}")
+        puts @break
+      end
+      print("Comparing '#{string1}' & '#{string2}':")
+      if tester.identical
+        print('These are exactly the same')
+      elsif tester.anagram_test
+        print('These are anagrams')
+      elsif tester.is_antigram
+        print('These words have no letter matches and are antigrams')
+      else
+        print('These are not anagrams')
+        print("However #{tester.match_count} of their letters matched:")
+        print("#{tester.matched_letters.join(', ')}")
+      end
+    else
+      print('Please only enter real words')
     end
     puts @break
-    puts "  Comparing '#{string1}' & '#{string2}':"
-    tester.anagram_test.each{ |line| puts "  #{line}"}
-    puts @break
+  end
+
+  def print(text)
+    puts "  #{text}"
   end
 
   def go_again
@@ -73,6 +90,5 @@ class UI
       |______|_| |_|\\___)_|\\_|| |_|   \\_||_|_|_|_|
                           (_____|
   "
-
   end
 end
