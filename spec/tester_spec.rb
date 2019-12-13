dd2891d60e25724da7eab218440348895832f26d
LiamPKenna <liampkenna@gmail.com>
2019-12-13 10:40:49 -0800

    Refactor test to make sure only single matched letters are counted

--- spec/tester_spec.rb
+++ spec/tester_spec.rb
@@ -18,7 +18,9 @@ describe('#Tester') do
 
   it('should test for the number of matching characters in the two strings') do
     tester = Tester.new('word', 'door')
-    expect(tester.matching_letters).to(eq(3))
+    expect(tester.matching_letters).to(eq([3,['d','o','r']]))
+    tester = Tester.new('door', 'word')
+    expect(tester.matching_letters).to(eq([3,['d','o','r']]))
   end
 
 end
